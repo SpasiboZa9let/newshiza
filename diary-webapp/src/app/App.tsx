3dc3dd553e903d090197e92ecd2cfa1334877b73
Иван <foxneversleep@gmail.com>
2025-09-29 09:40:28 +0300

    Add files via upload

---
+++ diary-webapp/src/app/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App: React.FC = () => {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1>Diary WebApp</h1>
+      <p>Заглушка: приложение запущено 🚀</p>
+    </div>
+  )
+}
+
+export default App
